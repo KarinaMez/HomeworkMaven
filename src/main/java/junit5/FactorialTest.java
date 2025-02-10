e5011e8f8126545b6543b3718ba72825a80ced98
KarinaM <mezencevakarina@mai.ru>
2025-02-10 19:57:26 +0300

    Maven

---
+++ src/main/java/junit5/FactorialTest.java
@@ -0,0 +1,22 @@
+package junit5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FactorialTest {
+
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(1, Factorial.factorial(0));
+    }
+
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        assertEquals(120, Factorial.factorial(5));
+    }
+
+    @Test
+    public void testFactorialOfNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(-5));
+    }
+}
